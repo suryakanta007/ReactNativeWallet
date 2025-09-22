ed287179d92c9d5228b79b3ec9385cff04ee207a
suryakanta007 <sahusuryakanta073@gmail.com>
2025-09-22 16:46:46 +0530

    add signup screen

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,15 @@
+import {Slot } from "expo-router";
+import SafeScreen from "@/components/SafeScreen"
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  );
+}
